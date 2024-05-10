48fcc79a6c8b89e26db63c86cca29a16a777a721
Ismytv <ismytv@gmail.com>
2024-05-09 22:06:30 -0500

    first commit: docker compose postgres

---
+++ src/app/api/hello/route.ts
@@ -0,0 +1,10 @@
+export async function GET(request: Request) {
+    return new Response(
+        JSON.stringify({
+            message: "Hello World",
+        }),
+        {
+            status: 200,
+        }
+    );
+}
