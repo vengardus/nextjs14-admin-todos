96a8fdbc98b784c8cf3e4a87488812183689a494
Ismytv <ismytv@gmail.com>
2024-05-20 21:24:37 -0500

    feat:section13: auth.js: google provider

--- src/auth.ts
+++ src/auth.ts
@@ -1,9 +1,9 @@
 import NextAuth from "next-auth";
 
 import GitHub from "next-auth/providers/github"
-//import Google from "next-auth/providers/google"
+import Google from "next-auth/providers/google"
  
 export const { auth, handlers, signIn, signOut } = NextAuth({
-  //providers: [GitHub, Google],
-  providers: [GitHub],
+  providers: [Google, GitHub],
+  //providers: [GitHub],
 })
